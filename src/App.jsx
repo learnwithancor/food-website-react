1fb0ae427c07388f0bd7c8977e6fb34681b251f0
learnwithancor <ancorpoint0@gmail.com>
2023-07-03 19:51:29 +0600

    commited

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Navbar/Hero";
+import HeadlineCards from "./components/Navbar/HeadlineCards";
+
+const App = () => {
+  return (
+    <>  
+        <Navbar />
+        <Hero />
+        <HeadlineCards />
+    </>
+  );
+};
+
+export default App;
